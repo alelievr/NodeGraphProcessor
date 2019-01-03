b5bbe515b69ffd69c620760b250f5f5839a9e050
alelievr <meadjix@gmail.com>
2019-01-03 15:05:05 +0100

    Added package.json

---
+++ Assets/Examples/Editor/03_CustomToolbar/CustomToolbarGraphView.cs
@@ -0,0 +1,30 @@
+using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+using GraphProcessor;
+using System;
+
+public class CustomToolbarGraphView : BaseGraphView
+{
+	public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+	{
+		evt.menu.AppendSeparator();
+		
+		foreach (var nodeMenuItem in NodeProvider.GetNodeMenuEntries())
+		{
+			Vector2 nodePosition = evt.mousePosition - (Vector2)viewTransform.position;
+			evt.menu.AppendAction("Create/" + nodeMenuItem.Key,
+				(e) => CreateNodeOfType(nodeMenuItem.Value, nodePosition),
+				DropdownMenu.MenuAction.AlwaysEnabled
+			);
+		}
+		
+		base.BuildContextualMenu(evt);
+	}
+
+	void CreateNodeOfType(Type type, Vector2 position)
+	{
+		RegisterCompleteObjectUndo("Added " + type + " node");
+		AddNode(BaseNode.CreateFromType(type, position));
+	}
+}

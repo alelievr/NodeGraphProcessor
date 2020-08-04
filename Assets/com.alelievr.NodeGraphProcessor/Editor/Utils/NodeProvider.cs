4ee5189b0b3dcb22b13b645c808fdd5dc75703fb
Antoine Lelièvre <antoinel@unity3d.com>
2020-08-04 20:14:35 +0200

    Added a method to tell that some types can't be casted into other types

--- Assets/com.alelievr.NodeGraphProcessor/Editor/Utils/NodeProvider.cs
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/Utils/NodeProvider.cs
@@ -1,41 +1,82 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
-using UnityEngine.UIElements;
 using System.Linq;
 using System.IO;
 using System.Reflection;
+using UnityEditor.Experimental.GraphView;
 
 namespace GraphProcessor
 {
 	public static class NodeProvider
 	{
+		public struct PortDescription
+		{
+			public Type nodeType;
+			public Type portType;
+			public bool isInput;
+			public string portFieldName;
+			public string portIdentifier;
+			public string portDisplayName;
+		}
+
 		static Dictionary< Type, Type >			nodeViewPerType = new Dictionary< Type, Type >();
 		static Dictionary< string, Type >		nodePerMenuTitle = new Dictionary< string, Type >();
 		static Dictionary< Type, MonoScript >	nodeViewScripts = new Dictionary< Type, MonoScript >();
 		static Dictionary< Type, MonoScript >	nodeScripts = new Dictionary< Type, MonoScript >();
 		static List< Type >						slotTypes = new List< Type >();
+		static List< PortDescription >			nodeCreatePortDescription = new List<PortDescription>();
 
 		static NodeProvider()
 		{
-			foreach (var type in AppDomain.CurrentDomain.GetAllTypes())
+			foreach (var nodeType in TypeCache.GetTypesDerivedFrom<BaseNode>())
 			{
-				if (type.IsClass && !type.IsAbstract)
-				{
-					if (type.IsSubclassOf(typeof(BaseNode)))
-						AddNodeType(type);
-					if (type.IsSubclassOf(typeof(BaseNodeView)))
-						AddNodeViewType(type);
+				if (nodeType.IsAbstract)
+					continue;
 
-					foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-					{
-						if (field.GetCustomAttributes().Any(c => c is InputAttribute || c is OutputAttribute))
-							slotTypes.Add(field.FieldType);
-					}
+				AddNodeType(nodeType);
+
+				foreach (var field in nodeType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+				{
+					if (field.GetCustomAttribute<HideInInspector>() == null && field.GetCustomAttributes().Any(c => c is InputAttribute || c is OutputAttribute))
+						slotTypes.Add(field.FieldType);
 				}
-            }
+
+				ProvideNodePortCreationDescription(nodeType);
+			}
+
+			foreach (var nodeViewType in TypeCache.GetTypesDerivedFrom<BaseNodeView>())
+			{
+				if (!nodeViewType.IsAbstract)
+					AddNodeViewType(nodeViewType);
+			}
+		}
+
+		static void ProvideNodePortCreationDescription(Type nodeType)
+		{
+			var node = Activator.CreateInstance(nodeType) as BaseNode;
+			try {
+				node.InitializePorts();
+				node.UpdateAllPorts();
+			} catch (Exception) {}
+
+			foreach (var p in node.inputPorts)
+				AddPort(p, true);
+			foreach (var p in node.outputPorts)
+				AddPort(p, false);
+
+			void AddPort(NodePort p, bool input)
+			{
+				nodeCreatePortDescription.Add(new PortDescription{
+					nodeType = nodeType,
+					portType = p.fieldInfo.FieldType,
+					isInput = input,
+					portFieldName = p.fieldName,
+					portDisplayName = p.portData.displayName ?? p.fieldName,
+					portIdentifier = p.portData.identifier,
+				});
+			}
 		}
 
 		static void AddNodeType(Type type)
@@ -125,5 +166,18 @@ namespace GraphProcessor
 		}
 
 		public static List<Type> GetSlotTypes() => slotTypes;
+
+		public static List<PortDescription> GetEdgeCreationNodeMenuEntry(PortView portView)
+		{
+			return nodeCreatePortDescription.Where(n => {
+				if (portView.direction == Direction.Input && n.isInput || portView.direction == Direction.Output && !n.isInput)
+					return false;
+	
+				if (!BaseGraph.TypesAreConnectable(n.portType, portView.portType))
+					return false;
+
+				return true;
+			}).ToList();
+		}
 	}
 }

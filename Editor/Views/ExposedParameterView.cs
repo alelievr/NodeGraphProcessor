6022aae65162e8998fff4b7b8a7ad2133d8efb3c
Antoine Lelievre <antoinel@unity3d.com>
2021-04-06 17:26:39 +0200

    Fixed a nullref on undo parameters when graph is closed

--- Editor/Views/ExposedParameterView.cs
+++ Editor/Views/ExposedParameterView.cs
@@ -103,6 +103,7 @@ namespace GraphProcessor
             RegisterCallback<DragUpdatedEvent>(OnDragUpdatedEvent);
             RegisterCallback<DragPerformEvent>(OnDragPerformEvent);
             RegisterCallback<MouseDownEvent>(OnMouseDownEvent, TrickleDown.TrickleDown);
+            RegisterCallback<DetachFromPanelEvent>(OnViewClosed);
 
             UpdateParameterList();
 
@@ -111,6 +112,9 @@ namespace GraphProcessor
                 text = "+"
             });
         }
+
+        void OnViewClosed(DetachFromPanelEvent evt)
+            => Undo.undoRedoPerformed -= UpdateParameterList;
 
         void OnMouseDownEvent(MouseDownEvent evt)
         {

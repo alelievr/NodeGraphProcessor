584b1d3a7a5fb48a4575d79b7f9d717023346df5
Antoine Lelievre <antoinel@unity3d.com>
2021-07-06 00:09:39 +0200

    Added support of the InspectorName attribute

--- Assets/Examples/DefaultNodes/Nodes/UnityEventNode.cs
+++ Assets/Examples/DefaultNodes/Nodes/UnityEventNode.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using GraphProcessor;
-using System.Linq;
 using UnityEngine.Events;
 
 [System.Serializable, NodeMenuItem("Custom/Unity Event Node")]

35a0514379d252c019545b41525841eb76626440
Antoine Lelievre <antoinel@unity3d.com>
2021-07-14 16:38:04 +0200

    Added workaround for node view with list bug

---
+++ Assets/Examples/DefaultNodes/Nodes/ListNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphProcessor;
+using System.Linq;
+
+[System.Serializable, NodeMenuItem("Custom/List")]
+public class ListNode : BaseNode
+{
+	[Output(name = "Out")]
+	public Vector4				output;
+	
+	[Input(name = "In"), SerializeField]
+	public Vector4				input;
+
+	public List<GameObject>		objs = new List<GameObject>();
+
+	public override string		name => "List";
+
+	protected override void Process()
+	{
+		output = input;
+	}
+}

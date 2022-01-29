ecc4dc08ed51d6536bf4bfaa39f8767c6ff6a2bd
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-29 23:44:44 +0100

    Relaxed filtering of inactive property fields

--- Assets/com.alelievr.NodeGraphProcessor/Editor/Views/BaseNodeView.cs
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/Views/BaseNodeView.cs
@@ -835,7 +835,7 @@ namespace GraphProcessor
 			var nodeIndexString = nodeIndex.ToString();
 			foreach (var propertyField in this.Query<PropertyField>().ToList())
 			{
-				if(!propertyField.enabledSelf || propertyField.bindingPath == null)
+				if(propertyField.bindingPath == null)
 					continue;
 				
 				propertyField.Unbind();

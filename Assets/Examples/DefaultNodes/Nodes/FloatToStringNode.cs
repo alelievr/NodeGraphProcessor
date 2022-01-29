781256693b38786ab9bf66cbcebbabe65e5666eb
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-29 23:19:51 +0100

    Add example conversion node

---
+++ Assets/Examples/DefaultNodes/Nodes/FloatToStringNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using GraphProcessor;
+using UnityEngine;
+
+[Serializable, NodeMenuItem("Convert/Float to String"), ConverterNode(typeof(float), typeof(string))]
+public class FloatToStringsNode : BaseNode, IConversionNode
+{
+	[Input("In")]
+	public float input;
+
+	public int decimalPlaces = 2;
+
+	[Output("Out")]
+	public string output;
+
+	public override string name => "To String";
+
+	public string GetConversionInput()
+	{
+		return nameof(input);
+	}
+
+	public string GetConversionOutput()
+	{
+		return nameof(output);
+	}
+
+	protected override void Process()
+	{
+		var mult = Mathf.Pow(10, decimalPlaces);
+		float val = Mathf.Round(input * mult) / mult;
+		output = val.ToString(CultureInfo.InvariantCulture);
+	}
+}
